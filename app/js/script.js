91fbffbf992fd0a0177d244b6f58b0a13c784f9d
manvith22 <chekurthimanvith32@gmail.com>
2021-10-15 07:13:28 +0530

    mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,24 @@
-console.log("hello World!");
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+
+btnHamburger.addEventListener('click',function(){
+    console.log('click hamburger');
+
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+        fadeElements.forEach(function(element){
+        element.classList.remove('fade-in');
+        element.classList.add('fade-out');
+        });
+    }
+    else{
+        header.classList.add('open');
+        fadeElements.forEach(function(element){
+        element.classList.remove('fade-out');
+        element.classList.add('fade-in');
+    });
+    }
+});
